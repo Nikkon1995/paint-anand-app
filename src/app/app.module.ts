e45dde719e489d531f5223e371307cf9ba72ffa6
Nikkon1995 <dutta.nikkon@gmail.com>
2021-12-09 19:43:07 +0530

    Made changes for proper view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponentComponent } from './app-main/navbar-component/navbar-component.component';
+import { BodyComponentComponent } from './app-main/body-component/body-component.component';
+import { FooterComponentComponent } from './app-main/footer-component/footer-component.component';
+import { AppMainComponent } from './app-main/app-main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponentComponent,
+    BodyComponentComponent,
+    FooterComponentComponent,
+    AppMainComponent
   ],
   imports: [
     BrowserModule,
