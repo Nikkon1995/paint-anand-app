d3a3303b735fdaa27e5a551a06170642f56b8310
Nikkon1995 <dutta.nikkon@gmail.com>
2022-01-25 02:38:19 +0530

    Pricing design complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,10 @@ import { AppComponent } from './app.component';
 import { AppMainComponent } from './app-main/app-main.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-import { RouterModule } from '@angular/router';
 import { PicturesComponent } from './pictures/pictures.component';
 import { FormsModule } from '@angular/forms';
 import { ContactComponent } from './contactForm/contact.component';
+import { AlertComponent } from './alert/alert.component';
 
 @NgModule({
   declarations: [
@@ -18,14 +18,14 @@ import { ContactComponent } from './contactForm/contact.component';
     HeaderComponent,
     FooterComponent,
     PicturesComponent,
-    ContactComponent
+    ContactComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
-    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
