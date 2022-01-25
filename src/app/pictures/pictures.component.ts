82aacff49a14ab483aad32e6365c1c477178a570
Nikkon1995 <dutta.nikkon@gmail.com>
2022-01-25 18:58:27 +0530

    Added animation for images folder

--- src/app/pictures/pictures.component.ts
+++ src/app/pictures/pictures.component.ts
@@ -1,11 +1,18 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
     selector: 'app-picture',
     templateUrl: './pictures.component.html',
     styleUrls: ['./pictures.component.css']
 })
-export class PicturesComponent {
+export class PicturesComponent implements OnInit{
+    animate = true;
+    ngOnInit(): void {
+        setTimeout(()=> {
+            this.animate = false;
+        }, 2000)
+    }
+
     typesOfWork: string[] = [
         'Repair',
         'Painting',
